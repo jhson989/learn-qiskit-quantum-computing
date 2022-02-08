e4b5680a85492f136217f8c08ec8825374144d70
Janghyun Son <jhson989@gmail.com>
2022-02-08 10:36:20 +0900

    Add first qiskit example

---
+++ ex-01/circuit.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from qiskit import QuantumCircuit, execute, Aer
+from qiskit.visualization import plot_histogram
+
+circuit = QuantumCircuit(4, 4)
+circuit.x(0)
+circuit.h(1)
+circuit.h(3)
+
+circuit.measure([0, 1, 2, 3], [0, 1, 2, 3])
+circuit.draw()
+
+backend = Aer.get_backend('qasm_simulator')
+results = execute(circuit, backend).result()
+counts = results.get_counts()
+
+print(counts)
+plot_histogram(counts)
+plt.show()
+
